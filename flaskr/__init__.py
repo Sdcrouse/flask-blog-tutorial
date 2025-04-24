9a7eb02c6d2b65a2d2aaa7a5e47c73aa770b33d1
Sdcrouse <sdcrouse91@gmail.com>
2025-04-23 21:43:41 -0700

    Added a blog blueprint and index file
    DO NOT REVERT TO THIS COMMIT OR ANY EARLIER ONES!
    Before this commit, any redirects to an index page would generate an error because the index file had not been created yet.
    This commit adds that index page, but it also links to other pages that haven't been created yet.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -36,4 +36,11 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    # Import and register the blog blueprint
+    from . import blog
+    app.register_blueprint(blog.bp)
+
+    # Create an index view for blog posts
+    app.add_url_rule('/', endpoint='index')
+
     return app
