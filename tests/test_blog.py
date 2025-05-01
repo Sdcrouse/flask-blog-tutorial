96233882f81668c228cf2bc88096857e699dc71c
Sdcrouse <sdcrouse91@gmail.com>
2025-04-30 23:41:55 -0700

    Removed repetitive code from tests

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -3,23 +3,23 @@ from flaskr.db import get_db
 
 def test_index_logged_in(client, auth):
     auth.login()
-    response = client.get('/')
-    assert b"Log In" not in response.data
-    assert b"Register" not in response.data
-    assert b'Log Out' in response.data
-    assert b'href="/create"' in response.data
-    assert b'test title' in response.data
-    assert b'by test on 2018-01-01' in response.data
-    assert b'test\nbody' in response.data
-    assert b'href="/1/update"' in response.data
+    response_data = client.get('/').data
+    assert b"Log In" not in response_data
+    assert b"Register" not in response_data
+    assert b'Log Out' in response_data
+    assert b'href="/create"' in response_data
+    assert b'test title' in response_data
+    assert b'by test on 2018-01-01' in response_data
+    assert b'test\nbody' in response_data
+    assert b'href="/1/update"' in response_data
 
 def test_index_logged_out(client):
-    response = client.get('/')
-    assert b"Log In" in response.data
-    assert b"Register" in response.data
-    assert b'href="/create"' not in response.data
-    assert b'href="/1/update"' not in response.data
-    assert b'Log Out' not in response.data
+    response_data = client.get('/').data
+    assert b"Log In" in response_data
+    assert b"Register" in response_data
+    assert b'href="/create"' not in response_data
+    assert b'href="/1/update"' not in response_data
+    assert b'Log Out' not in response_data
 
 @pytest.mark.parametrize('path', (
     '/create',
