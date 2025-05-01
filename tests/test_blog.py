c01c09e3da1c6acd9b54a8857634436894dc9b12
Sdcrouse <sdcrouse91@gmail.com>
2025-04-30 23:28:37 -0700

    Added additional tests to ensure that certain buttons are only visible on the index page when logged in/out

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -5,10 +5,16 @@ def test_index(client, auth):
     response = client.get('/')
     assert b"Log In" in response.data
     assert b"Register" in response.data
+    assert b'href="/create"' not in response.data
+    assert b'href="/1/update"' not in response.data
+    assert b'Log Out' not in response.data
 
     auth.login()
     response = client.get('/')
+    assert b"Log In" not in response.data
+    assert b"Register" not in response.data
     assert b'Log Out' in response.data
+    assert b'href="/create"' in response.data
     assert b'test title' in response.data
     assert b'by test on 2018-01-01' in response.data
     assert b'test\nbody' in response.data
